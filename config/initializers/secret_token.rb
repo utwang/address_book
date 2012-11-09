ce88e8f700bce7f0c49e056024e987f07fef22a0
utwang <x2x2tone@gmail.com>
2012-11-09 15:11:46 +0900

    create Projcet & install initial gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressBook::Application.config.secret_token = '3c54b4ee72cfa42f4273e0cb5050df87fd554b54f1596413782fae8405a83d035edc1b1e7bae34062fbe7ce6c140b760f0d292420bd71315905642ba5344a9d8'
