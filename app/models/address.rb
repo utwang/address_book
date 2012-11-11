b1a7f2783fc2897743076ffdb400f603cad90c9f
utwang <x2x2tone@gmail.com>
2012-11-11 17:30:42 +0900

    implemented asscoiation refs #task0603

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ActiveRecord::Base
-  attr_accessible :address_list_id, :city, :country, :email, :first_name, :last_name, :organization, :phone, :postal_code, :prefecture, :street_address1, :street_address2
+  belongs_to :address_list
+
+  attr_accessible :address_list_id, :city, :country, :email, :first_name, :last_name,
+                  :organization, :phone, :postal_code, :prefecture, :street_address1, :street_address2
 end
--- app/models/address_list.rb
+++ app/models/address_list.rb
@@ -1,3 +1,4 @@
 class AddressList < ActiveRecord::Base
+  has_many :addresses
   attr_accessible :description, :title, :user_id
 end
