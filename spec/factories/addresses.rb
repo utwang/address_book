a2ac94f0356fe77fbe5a07c882ba8090a4df7cc3
utwang <x2x2tone@gmail.com>
2012-11-11 16:32:02 +0900

    implemented factoy & spec for Address refs #task0602

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,18 +1,17 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+# coding: utf-8
 
 FactoryGirl.define do
   factory :address do
-    address_list_id 1
-    first_name "MyString"
-    last_name "MyString"
-    email "MyString"
-    organization "MyString"
-    country "MyString"
-    street_address1 "MyText"
-    street_address2 "MyText"
-    city "MyString"
-    prefecture "MyString"
-    postal_code "MyString"
-    phone "MyString"
+    first_name "一郎"
+    last_name "田中"
+    email "ichiro_t@example.co.jp"
+    organization "日本"
+    country "日本"
+    street_address1 "渋谷1丁目"
+    street_address2 "シブヤビル21F"
+    city "渋谷区"
+    prefecture "東京都"
+    postal_code "123-0012"
+    phone "03-1001-1001"
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,25 @@
+# coding: utf-8
+
 require 'spec_helper'
 
 describe Address do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association' do
+    it { should belong_to(:address_list) }
+  end
+
+  describe 'validation' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:phone) }
+
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_format_of(:email).with('xyz.abc@example.com') }
+    it { should validate_format_of(:email).not_with('xyz.abc@example')
+                                          .with_message(/無効なメールアドレス/) }
+    it { should validate_format_of(:email).not_with('xyz_abc@example.com')
+                                          .with_message(/無効なメールアドレス/) }
+    it { should validate_format_of(:email).not_with('xyz.abcexample.com')
+                                          .with_message(/無効なメールアドレス/) }
+  end
 end
