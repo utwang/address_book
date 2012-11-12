632fb8a7fe8b1a24544044b9cb653e1c595387ae
utwang <x2x2tone@gmail.com>
2012-11-12 14:05:20 +0900

    add form for creating a new list refs #task0702

--- app/controllers/address_lists_controller.rb
+++ app/controllers/address_lists_controller.rb
@@ -1,5 +1,8 @@
+# coding: utf-8
+
 class AddressListsController < ApplicationController
   def index
+    #TODO user_id対応
     user_id = 1
     @address_lists = AddressList.where(user_id: user_id)
     @new_list = AddressList.new(user_id: user_id)
@@ -7,11 +10,16 @@ class AddressListsController < ApplicationController
 
   def create
     address_list = AddressList.new(params[:address_list])
+    #TODO user_id対応
+    address_list.user_id = 1
 
     if address_list.save
+      flash[:success] = "新しいアドレス帳を登録しました"
       redirect_to address_lists_path
     else
-      render 'index'
+      #TODO 複数行表示の対応
+      flash[:error] = "エラー: #{address_list.errors.messages}"
+      redirect_to address_lists_path
     end
   end
 end
--- spec/requests/address_list_index_pages_spec.rb
+++ spec/requests/address_list_index_pages_spec.rb
@@ -7,13 +7,41 @@ describe "AddressListIndexPages" do
 
   let!(:address_list0) { FactoryGirl.create(:address_list, user_id: 1, title: 'プライベート', description: '友人') } 
   let!(:address_list1) { FactoryGirl.create(:address_list, user_id: 1, title: '仕事の取引先') } 
+  before { visit address_lists_path }
 
   describe "GET /address_list_index_pages" do
-    before { visit address_lists_path }
-  
     it { should have_css('#address_list_0 td.list_title', text: 'プライベート') }
     it { should have_css('#address_list_0 td.list_desc', text: '友人') }
     #TODO アドレス一覧作成後にリンク先のパス変更
     it { should have_link('プライベート', href: '#') }
+  end
+
+  describe "create a new address_list" do
+    let(:new_list_title) { '新アドレス帳' }
+    let(:new_list_description) { '新規のアドレス帳' }
+
+    context 'OK' do
+      before do
+        fill_in 'アドレス帳名', with: new_list_title
+        fill_in '概要', with: new_list_description
+        click_button 'Create'
+      end
+
+      it { should have_content('新しいアドレス帳を登録しました') }
+      it { should have_css('#address_list_2 td.list_title', text: '新アドレス帳') }
+      it { should have_css('#address_list_2 td.list_desc', text: '新規のアドレス帳') }
+    end
+
+    context 'NG' do
+      let!(:address_list2) { FactoryGirl.create(:address_list, user_id: 1, title: '3つ目のアドレス帳') }
+
+      before do
+        fill_in 'アドレス帳名', with: new_list_title
+        fill_in '概要', with: new_list_description
+        click_button 'Create'
+      end
+
+      it { should have_content('エラー') }
+    end
   end
 end
