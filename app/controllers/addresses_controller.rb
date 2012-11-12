2f9ba54d115a952bb75b2c8a5a026be79e0f5188
utwang <x2x2tone@gmail.com>
2012-11-12 20:48:00 +0900

    refactoring page appearance(title, link, button, table layout etc)　refs #task1104

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,7 @@
 class AddressesController < ApplicationController
   def index
     @addresses = Address.where(address_list_id: params[:address_list_id])
+    @address_list = AddressList.find(params[:address_list_id])
 
     respond_to do |format|
       format.html # index.html.erb
